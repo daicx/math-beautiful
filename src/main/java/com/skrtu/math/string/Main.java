2330c2fbb29ce55e45f9788809dffe0971e0d8ca
daichangxia <daichangxia@58.com>
2022-06-04 16:09:31 +0800

    DeleteRepeat

--- src/main/java/com/skrtu/math/string/Main.java
+++ src/main/java/com/skrtu/math/string/Main.java
@@ -1,22 +1,33 @@
 package com.skrtu.math.string;
 
+import com.skrtu.math.node.ListNode;
+
 public class Main {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode next = head.next;
+        if (next == null) {
+            return head;
+        }
+        if (head.val == next.val) {
+            while (next != null) {
+                if (next.val != head.val) {
+                    break;
+                }
+                next = next.next;
+            }
+            head = deleteDuplicates(next);
+        } else {
+            head.next = deleteDuplicates(next);
+        }
+        return head;
+    }
 
 
     public static void main(String[] args) {
-        String str = "adadc";
-        String winStr = "";
-        String resultStr = "";
-        for (int i = 0; i < str.length(); i++) {
-            String tmp = String.valueOf(str.charAt(i));
-            if (winStr.contains(tmp)) {
-                winStr = winStr.substring(winStr.indexOf(tmp)) + tmp;
-            } else {
-                winStr = winStr + tmp;
-            }
-            if (resultStr.length() < winStr.length()) {
-                resultStr = winStr;
-            }
-        }
+
     }
 }
