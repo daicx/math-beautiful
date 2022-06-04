ff5026f364236144d3cf6b54c02d6b166f870c43
daichangxia <daichangxia@58.com>
2022-06-04 18:12:33 +0800

    链表反转

--- src/main/java/com/skrtu/math/node/ReverseList.java
+++ src/main/java/com/skrtu/math/node/ReverseList.java
@@ -4,43 +4,19 @@ package com.skrtu.math.node;
  * @Author dcx
  * @Description //链表反转
  * @Date 16:34 2020/6/3
+ * https://leetcode.cn/problems/reverse-linked-list/submissions/
  **/
 public class ReverseList {
-    class ListNode {
-        int val;
-        ListNode next;
 
-        public ListNode(int val) {
-            this.val = val;
-        }
-    }
-
-    /***
-     * @Author dcx
-     * @Description //双指针
-     *
-     * 时间复杂度 O(n)
-     * 空间复杂度 O(1)
-     * 执行用时 :0 ms, 在所有 Java 提交中击败了100.00%的用户
-     * 内存消耗 :39.5 MB, 在所有 Java 提交中击败了100.00%的用户
-     *
-     * @Date 16:34 2020/6/3
-     * @Param [head]
-     * @return com.skrtu.math.node.ReverseList.ListNode
-     **/
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
-        ListNode cur = head;
-        ListNode next;
-        while (cur != null) {
-            //保存下一个节点
-            next = cur.next;
-            //将下一个节点指向pre
+        ListNode cur;
+        while (head != null) {
+            ListNode next = head.next;
+            cur = head;
             cur.next = pre;
-            //右移pre
             pre = cur;
-            //右移cur
-            cur = next;
+            head = next;
         }
         return pre;
     }
