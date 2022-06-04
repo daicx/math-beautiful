edb593adf1073a6befb2ade86007117abf3a3dbc
daichangxia <daichangxia@58.com>
2022-06-04 15:54:35 +0800

    DeleteRepeat

--- src/main/java/com/skrtu/math/string/LengthOfLongestSubstring.java
+++ src/main/java/com/skrtu/math/string/LengthOfLongestSubstring.java
@@ -2,6 +2,7 @@ package com.skrtu.math.string;
 
 /***
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * https://leetcode.cn/problems/longest-substring-without-repeating-characters/
  **/
 public class LengthOfLongestSubstring {
 
