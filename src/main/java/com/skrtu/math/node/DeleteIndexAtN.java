70bdea94621cb8b9a39ce30528ead068d605a948
daichangxia <daichangxia@58.com>
2022-06-04 19:46:17 +0800

    removeNthFromEnd

---
+++ src/main/java/com/skrtu/math/node/DeleteIndexAtN.java
@@ -0,0 +1,23 @@
+package com.skrtu.math.node;
+
+/**
+ * https://leetcode.cn/problems/remove-nth-node-from-end-of-list/
+ */
+public class DeleteIndexAtN {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode first = head;
+        ListNode listNode = new ListNode(0, head);
+        ListNode second = listNode;
+        int count = 0;
+        while (first != null) {
+            count++;
+            if (count > n){
+                second = second.next;
+            }
+            first = first.next;
+        }
+        second.next = second.next.next;
+        return listNode.next;
+    }
+}
